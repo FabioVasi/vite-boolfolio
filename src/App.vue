9fcbc35cc232237266a9e552404f1bb5f01364ae
FabioVasi <vasifabio96@gmail.com>
2023-11-17 16:56:50 +0100

    add bootstrap and sass

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      base_url: 'http://127.0.0.1:8000',
+      api_url: '/api/projects',
+      projects: []
+    }
+  },
+  mounted() {
+    axios
+    .get('')
+    .then(response => {
+      console.log(response);
+      this.projects = response.data.result
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <header>
+
+  </header>
+
+  <main>
+    <div class="container">
+      
+    <h1>Hello</h1>
+
+      <div class="row g-3">
+        <div class="col-3" v-for="project in projects.data">
+          <div class="card">
+            <h4>{{ project.title }}</h4>
+            <div class="card-body"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+
+  </footer>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
