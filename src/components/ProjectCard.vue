80419e62086b4b8462370a269c89b603ba70d20c
FabioVasi <vasifabio96@gmail.com>
2023-11-21 21:22:18 +0100

    add list and card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,13 +1,28 @@
 
 <script>
+import { state } from '../state.js';
+
 export default {
-  
+  name: 'ProjectCard',
+  props: ['project'],
+  data() {
+    return {
+      state
+    }
+  }
 }
 </script>
 
 <template>
-  <div>
-
+  <div class="col">
+    <div class="card">
+      <img class="card-img-top" :src="project.image" alt="">
+      <div class="card-body">
+        <h3>{{ project.title }}</h3>
+        <p>{{ project.content }}</p>
+        <router-link :to="{name: 'project', params: {slug: project.slug}}">View</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,12 +1,73 @@
 <script>
+import axios from 'axios';
+import { state } from "../state.js";
+import ProjectCard from "./ProjectCard.vue";
+
 export default {
-    
+    name: 'ProjectList',
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            state,
+            blog_api: '/api/projects',
+            projects: null
+        }
+    },
+    methods: {
+        getProjects() {
+            const url = this.state.base_url + this.blog_url;
+            console.log(url);
+            axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+                this.projects = response.data.result
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
     <div>
+        <section v-if="projects">
+            <div class="container">
+                <div class="row">
+                    <ProjectCard :project="project" v-for="project in projects.data"></ProjectCard>
+                </div>
+            </div>
 
+            <div class="container">
+                <div class="pagination my-3">
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination    ">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
