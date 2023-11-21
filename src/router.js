c882e82de0bf0c8651b34c0e42cf06d0ee2e4d05
FabioVasi <vasifabio96@gmail.com>
2023-11-21 15:48:27 +0100

    Add view components

--- src/router.js
+++ src/router.js
@@ -2,12 +2,21 @@ import { createWebHashHistory, createRouter } from "vue-router";
 
 import HomeView from "./views/HomeView.vue";
 import AboutView from "./views/AboutView.vue";
+import BlogView from "./views/BlogView.vue";
 import ContactsView from "./views/ContactsView.vue";
+import SingleProjectView from "./views/SingleProjectView.vue";
+import SingleTypeView from "./views/SingleTypeView.vue";
+import SingleTechnologyView from "./views/SingleTechnologyView.vue";
+
+import NotFoundView from "./views/NotFoundView.vue";
 
 const routes = [
     {path: '/', name: 'Home', component: HomeView},
     {path: '/About', name: 'About', component: AboutView},
+    {path: '/Blog', name: 'Blog', component: BlogView},
     {path: '/Contacts', name: 'Contacts', component: ContactsView},
+
+    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView}
 ];
 
 const router = createRouter({
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'NotFoundView'
+}
+</script>
+
+<template>
+    <div>
+
+        <h1>404 Not Found!</h1>
+
+        <router-link :to="{name: 'Home'}">Back to Home Page</router-link>
+
+    </div>
+</template>
+
+<style>
+
+</style>
